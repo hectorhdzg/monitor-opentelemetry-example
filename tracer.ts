eddfcebf3d12aba7300dae6fc904a63d341b0eb4
Hector Hernandez Guzman <hectorh@microsoft.com>
2021-09-14 10:59:48 -0700

    Add docker

--- tracer.ts
+++ tracer.ts
@@ -1,17 +1,26 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { NodeTracerProvider } from "@opentelemetry/node";
 import { AzureMonitorTraceExporter } from "@azure/monitor-opentelemetry-exporter";
+import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { MySQLInstrumentation } from "@opentelemetry/instrumentation-mysql";
-import { SimpleSpanProcessor } from "@opentelemetry/tracing";
+import { ConsoleSpanExporter, SimpleSpanProcessor } from "@opentelemetry/tracing";
+
 
 export function getOpenTelemetryTracer() {
     const provider = new NodeTracerProvider();
-    const exporter = new AzureMonitorTraceExporter();
-    provider.addSpanProcessor(new SimpleSpanProcessor(exporter as any));
+    const azureExporter = new AzureMonitorTraceExporter();
+    // Configure span processor to send spans to the exporter
+    const jaegerExporter = new JaegerExporter({
+        endpoint: 'http://localhost:14268/api/traces',
+    });
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(azureExporter));
+    provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
     // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
     provider.register();
     registerInstrumentations({
